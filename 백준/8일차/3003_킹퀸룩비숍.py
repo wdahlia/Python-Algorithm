087edc8fdb418cf7011d827cac9d2985f213d6c1
dahlia <wlstnrdahlia@gmail.com>
2022-08-04 23:36:29 +0900

    Add : 백준 실습 8일차 문제  업로드

---
+++ 백준/8일차/3003_킹퀸룩비숍.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('3003_킹퀸룩비숍.txt')
+
+chess = [1, 1, 2, 2, 2, 8]
+chess_n = list(map(int, input().split()))
+
+for i in range(6):
+    if chess[i] != chess_n[i]:
+        chess[i] = chess[i] - chess_n[i]
+    else:
+        chess[i] = 0
+
+print(*chess, sep=' ')
---
+++ 백준/8일차/9455_박스.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('9455_박스.txt')
+
+from pprint import pprint
+
+
+T = int(input())
+
+for _ in range(T):
+    n, m = list(map(int, input().split()))
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    
+    sum_cnt = 0
+    for col in range(m):
+        cnt = 0
+        for row in range(n):
+            if matrix[row][col] == 1:
+                cnt += 1
+            else:
+                sum_cnt += cnt
+    
+    print(sum_cnt)
+
+
