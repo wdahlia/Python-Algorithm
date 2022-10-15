ce796a0398b27fc1291690218e9a0b431b70dd6f
dahlia <wlstnrdahlia@gmail.com>
2022-10-15 22:43:42 +0900

    Add : BOJ 2606 BFS 풀이 추가

--- KDT 실습/0809 BOJ_N/2606_바이러스.py
+++ KDT 실습/0809 BOJ_N/2606_바이러스.py
@@ -40,4 +40,35 @@ while stop:
     if len(result) == length: # 즉, 값이 추가가 안된다는 소리
         stop = False
 print(len(result) - 1) # 시작 인덱스를 하나 제외해주어야 함
-                
+
+# bfs
+from collections import deque
+
+input = sys.stdin.readline
+
+com = int(input()) # 컴퓨터 수(정점) 
+n= int(input()) # 컴퓨터 쌍의 수(간선)
+
+graph = [[] for _ in range(com+1)] # 컴퓨터 수만큼의 그래프
+visited = [False]*(com+1) #노드가 방문된 정보
+
+for _ in range(n): # 컴퓨터 쌍의 수만큼 반복
+    a, b = map(int,input().split()) # 컴퓨터 쌍 input
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(graph, start, visited):
+    queue = deque([start]) #현재 노드 방문 처리
+    visited[start] = True
+    cnt = 0
+    while queue: 
+        v = queue.popleft() # 큐에서 값 하나씩 봅아서
+
+        for i in graph[v]:
+            if not visited[i]: #변수가 방문하지 않았다면
+                queue.append(i)
+                visited[i] = True
+                cnt += 1
+    return cnt
+
+print(bfs(graph, 1, visited))
