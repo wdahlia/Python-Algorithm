828c6da0f67f441e81f45b178591b8aff3771d84
dahlia <wlstnrdahlia@gmail.com>
2023-01-18 15:24:11 +0900

    add : BOJ 1260 DFS와 BFS 문제풀이 업로드

--- 백준/1260_dfsbfs.py
+++ 백준/1260_dfsbfs.py
@@ -8,32 +8,61 @@ from collections import deque
 # 간선의 개수 M
 # 탐색시작 정점의 번호 V
 # 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(M+1)] 
-visited = [False] * (M+1)
 
-for _ in range(M):
-    i, j = map(int, input().split())
-    graph[i].append(j)
-    graph[j].append(i)
-
-for i in range(M+1):
-    graph[i].sort()
-
-
-
+# dfs 코드
 def dfs(start):
-    visited[start] = True
+    dfsVisited[start] = True
     stack = [start]
-    # print(start, end=' ')
+    result.append(start)
 
     while len(stack) != 0:
         point = stack.pop()
 
-        for current in graph[point]:
-            # print(current)
-            if not visited[current]:
-                visited[current] = True
-                stack.append(current)
-                print(stack)
-dfs(V)
+        for current in dfsGraph[point]:
+            if not dfsVisited[current]:
+                dfsVisited[current] = True # 방문처리를 해준다음 재귀함수 사용해서 그 해당 인덱스 노드 탐색
+                dfs(current)
+
+    return result
+
+# bfs 코드 queue를 사용
+def bfs(start):
+    bfsVisited[start] = True
+    array = deque([start])
+    result = []
+    result.append(start)
+    
+    while len(array) != 0:
+        point = array.popleft()
+
+        for current in bfsGraph[point]:
+            if not bfsVisited[current]:
+                bfsVisited[current] = True
+                array.append(current)
+                result.append(current)
+
+    return result
+
+
+N, M, V = map(int, input().split())
+dfsGraph = [[] for _ in range(N+1)] 
+dfsVisited = [False] * (N+1)
+bfsGraph = [[] for _ in range(N+1)]
+bfsVisited = [False] * (N+1)
+result = []
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    dfsGraph[i].append(j)
+    dfsGraph[j].append(i)
+    bfsGraph[i].append(j)
+    bfsGraph[j].append(i)
+
+for i in range(N+1):
+    dfsGraph[i].sort()
+    bfsGraph[i].sort()
+
+
+                
+print(*dfs(V))
+print(*bfs(V))
